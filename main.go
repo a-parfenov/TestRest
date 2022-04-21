040a97b4799ae8c7c82a1ad04b76563b72e6f248
Parfenov <v5pishka@yandex.ru>
2022-04-21 21:32:10 +0300

    Test

--- main.go
+++ main.go
@@ -1,27 +1,34 @@
 package main
 
 import (
+	"bytes"
 	"container/list"
 	"fmt"
 	"html/template"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
-	// "io"
+	// "strings"
 	// "bytes"
 	// "strings"
 )
 
 type node struct {
 	name	string
-	list	list.List
+	save	map[string] bool
+	// list	list.List
 	// next	*node
 }
 
 func readFileChunkWise(w http.ResponseWriter, r *http.Request) {
-	b := make([]byte, 1048576)  // обработка 1048576 байтов файла
+	var size int = 300
+	b := make([]byte, size)  // обработка 1048576 байтов файла
+	// for i := 0; i < size; i++ {
+	// 	b[i] = 0
+	// }
 	file1, header, err := r.FormFile("file")
 	file1.Close()
 	file, err := os.Open(header.Filename);
@@ -35,6 +42,9 @@ func readFileChunkWise(w http.ResponseWriter, r *http.Request) {
 	os.Mkdir("createdDir", 0777)
 	i := 1
 	for {
+		for i := 0; i < size; i++ {
+			b[i] = 0
+		}
 		bytesRead, _ := file.Read(b);
 		if bytesRead == 0 { // bytesRead будет равен 0 в конце файла.
 			break
@@ -49,8 +59,16 @@ func readFileChunkWise(w http.ResponseWriter, r *http.Request) {
 	for e := list.Front(); e != nil; e = e.Next() {
 		fmt.Println(e.Value)  // печать списка файлов по 1 мб
 	}
+	// strings.TrimRight()
+	// strings.TrimPrefix()
+	// bytes.NewBuffer()
+	
 	file.Close();
-	postform(w, r)
+	io.WriteString(w, `
+		<form >
+			<h3>Файл сохранен</h3>
+		</form>
+		`)
 }
 
 func process(b []byte, bytesRead int, dirName string) {
@@ -60,22 +78,62 @@ func process(b []byte, bytesRead int, dirName string) {
 	}
 }
 
-
+func existFinc(path string) (bool) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true }
+	if os.IsNotExist(err) {
+		return false }
+	return false
+}
 
 func postform(w http.ResponseWriter, r *http.Request) {
-
 	t, _ := template.ParseFiles("sources_html/postform.html")
 	t.Execute(w, "")
 }
 
 func download(w http.ResponseWriter, r *http.Request) {
-
 	t, _ := template.ParseFiles("sources_html/download.html")
+	t.Execute(w, "")
+
+	// if existFinc("createdDir"){
+	// 	fmt.Fprintf(w, "Сборка и сохранение файла")
+
+		// _, header, _ := r.FormFile("text")
+		// fmt.Fprintf(w, header.Filename)
+		// _, err := os.Open("createdDir/1-" + header.Filename);
+		// if err != nil {
+		// 	fmt.Fprintf(w, "Такой файл не сохранен")
+		// 	return
+		// } else {
+			// b := make([]byte, 1048576)
+			// for i := 0; i < 1000; i++ {
+			// 	file, err := os.Open("createdDir/" + strconv.Itoa(i) + "-" + header.Filename);
+			// 	if err != nil { break }
+
+			// 	i := 1
+			// 	for {
+			// 		bytesRead, _ := file.Read(b);
+			// 		if bytesRead == 0 { break }
+			// 		dirName := header.Filename
+			// 		process(b, bytesRead, dirName);
+			// 		i++
+			// 	}
+			// }
+		}
+
+	// } else {
+	// 	io.WriteString(w, `
+	// 	<form >
+	// 		<h3>Файл не сохранен</h3>
+	// 	</form>
+	// 	`)
+	// }
+
 	// file1, header, _ := r.FormFile("file")
 	// file1.Close()
 	// t.ExecuteTemplate(w, "", header.Filename)
-	t.Execute(w, "")
-}
+// }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 
@@ -84,10 +142,19 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("Метод:", r.Method)
 	if r.Method == "POST" {
+		// fmt.Fprintf(w, "go in post")
 		readFileChunkWise(w, r)
 	} else if r.Method == "GET" {
-		fmt.Println("go in get")
+		// fmt.Fprintf(w, "go in get")
+		// download(w, r)
 
+	   	// crutime := time.Now().Unix()
+	   	// h := md5.New()
+	   	// io.WriteString(h, strconv.FormatInt(crutime, 10))
+	   	// token := fmt.Sprintf("%x", h.Sum(nil))
+
+	   	// t, _ := template.ParseFiles("upload.gtpl")
+	   	// t.Execute(w, token)
 	} else {
 		fmt.Println("** error **")
 	}
